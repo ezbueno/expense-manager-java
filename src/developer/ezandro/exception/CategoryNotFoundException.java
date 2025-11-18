71ffa493ce7e3cf3b9f4b5d7252822db9d7e38c1
ezbueno <ezandrobueno@yahoo.com.br>
2025-11-18 16:11:17 -0300

    feat: add custom exception for category validation
    - Add CategoryNotFoundException for invalid category options
    - Extend RuntimeException for unchecked exception handling

---
+++ src/developer/ezandro/exception/CategoryNotFoundException.java
@@ -0,0 +1,7 @@
+package developer.ezandro.exception;
+
+public class CategoryNotFoundException extends RuntimeException {
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+}
